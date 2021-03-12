151004a2bd0e44681e6d7d5b8f1d7b00f1c8a6a8
Jan Szachno <j.szachno@gmail.com>
2021-03-12 21:05:28 +0100

    serving static files

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.send("Hello World")
+app.use(express.static('public'))
+
+app.get('/test', (req, res) => {
+  return res.send("testowa ścieżka")
 })
 
 const PORT = process.env.PORT || 8080
