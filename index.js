9c4fdd470c158b44f910ed663e960760bf0b8470
Mieszko <mirgosmieszko@gmail.com>
2021-03-12 23:35:50 +0100

    added edit page layout + basic routing

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/test', (req, res) => {
   return res.send("testowa ścieżka")
 })
 
+app.use('/index', (req, res)=>{
+  return res.sendFile('./public/index.html', {root: __dirname});
+});
+
+app.use((req, res)=>{
+  return res.sendFile('./public/edit.html', {root: __dirname});
+});
+
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
