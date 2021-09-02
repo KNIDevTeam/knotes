492302aaf8538782a61ebc4b0133e9c38471716f
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-09-02 16:02:11 +0200

    uploading current state Save button is currently not working sorry for the delay

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,44 +1,40 @@
-const json = require('body-parser/lib/types/json');
 const fs = require('fs');
+const { url } = require('inspector');
 const Note = require('../models/note');
 
+const PATH_PREFIX = './public/notes/'
+const PATH_SUFFIX = '.txt'
+
 const note_get = (req, res) => {
-    Note.find({},(err,result)=>{
-        if(err){
-            console.log(err);
+    Note.find({}, (error, result) => {
+        if (error) {
+            console.log(error);
         }
         else
         {
             console.log(result);
+            var filenames = []
+            for (var i = 0; i < result.length; i++) {
+                filenames.push(result[i].url.replace(PATH_PREFIX,'').replace(PATH_SUFFIX,''))
+            }
+            res.render('notes/index', { title: 'Wszystkie notatki', notes: filenames });
         }
     });
-    const filenames = fs.readdirSync('./public/notes').map(filename => {
-        return filename.substr(0, filename.length - 4)
-    });
-    res.render('notes/index', { title: 'Wszystkie notatki', notes: filenames });
 };
 
 const note_details = (req, res) => {
     const note_url = req.params.filename;
-    Note.find({url: note_url},(error,result)=>{
-        if(error){
+    Note.findOne({url: PATH_PREFIX + note_url + ".txt"}, (error, result) => {
+        if (error) {
             console.log(error);
             res.redirect('/404');
         }
         else
         {
-            data = result.content
-            res.render('notes/details', { name: note_url, body: data, title: 'Notatka'});
+            res.render('notes/details', { name: note_url, body: result.content, title: 'Notatka'});
+            console.log(result)
         }
     });
-    // try {
-
-    //     const data = fs.readFileSync('./public/notes/' + req.params.filename + '.txt');
-    //     res.render('notes/details', { name: req.params.filename, body: data, title: 'Notatka'});
-    // } catch (err) {
-    //     console.log(err);
-    //     res.redirect('/404');
-    // }
 };
 
 const note_create_get = (req, res) => {
@@ -46,18 +42,15 @@ const note_create_get = (req, res) => {
 };
 
 const note_create_post = (req, res) => {
-    const path = './public/notes/' + req.body.title + '.txt';
-    if(!fs.existsSync(path)) {
+    const path = PATH_PREFIX + req.body.title + PATH_SUFFIX;
+    if (!fs.existsSync(path)) {
         const note = new Note({
-            url:path,
-            content:req.body.text
+            url: path,
+            content: req.body.text
         })
         note.save()
-        .then((result) => console.log("success"))
-        .catch((err)=>console.log(err));
-        //
-        //fs.writeFileSync(path, req.body.text);
-        // jak db dziala to do wywalenia
+        .then(() => console.log("success"))
+        .catch((error) => console.log(error));
         res.redirect('/notes');
     } else {
         res.render('notes/create', { title: 'Stwórz notatkę', exists: true });
@@ -65,25 +58,29 @@ const note_create_post = (req, res) => {
 };
 
 const note_delete = (req, res) => {
-    Note.deleteOne({url: './public/notes/' + req.params.filename + '.txt'},function(err,result) {
-        if (err) return console.log(err);
-       console.log("deleted one record");
+    Note.deleteOne({url: PATH_PREFIX + req.params.filename + PATH_SUFFIX}, function(error, result) {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log("deleted one record");
+            res.json({ redirect: '/notes' });
+        }
     })
-    //jak db działa to do wywalenia
-    fs.rmSync('./public/notes/' + req.params.filename + '.txt');
-    //
-    res.json({ redirect: '/notes' });
 };
 
 const note_update = (req, res) => {
-    console.log(req.body)
-    const path = './public/notes/' + req.body.oldUrl + '.txt';
-
-    fs.writeFileSync(path, req.body.content)
-    res.redirect('/notes')
-}
-
-
+    Note.findOneAndUpdate({url: PATH_PREFIX + req.body.title + PATH_SUFFIX}, 
+        // {url: PATH_PREFIX + req.params.filename + PATH_SUFFIX, content: req.params.content })
+        function(error, result) {        
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(req)
+            result = { url: req.body.filename, content: req.body.content }
+        }
+        res.redirect('/notes')
+    })
+};
 
 module.exports = {
     note_get, 
